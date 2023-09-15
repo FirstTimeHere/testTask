c4ade5d81eef1cb2a6955cc8f6654b5b7f8b2e9e
FirstTimeHere <polshin2@gmail.com>
2023-09-15 23:15:18 +0500

    left/right

--- testTask/Assets/Scripts/CreateObject.cs
+++ testTask/Assets/Scripts/CreateObject.cs
@@ -6,14 +6,43 @@ public class CreateObject : MonoBehaviour
 {
     [SerializeField] 
     private GameObject[] items;
+    private int index;
 
     private void Start()
     {
-        for (int i = 0; i < items.Length; i++) 
-        { 
-            Debug.Log("Îáúåêòû"+ items[i].name); 
+        items = new GameObject[transform.childCount];
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            items[i]=transform.GetChild(i).gameObject;
         }
-        
+        foreach(GameObject item in items)
+        {
+            item.SetActive(false);
+        }
+        if (items[0])
+        {
+            items[0].SetActive(true);
+        }
+    }
 
+    public void LeftLoad()
+    {
+        items[index].SetActive(false);
+        index--;
+        if (index < 0)
+        {
+            index = items.Length - 1;
+        }
+        items[index].SetActive(true);
+    } 
+    public void RightLoad()
+    {
+        items[index].SetActive(false);
+        index++;
+        if (index == items.Length) 
+        {
+            index = 0;
+        }
+        items[index].SetActive(true);
     }
 }
